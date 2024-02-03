4e65fd6ded489f878eb154ec7bce665cefa0f287
Abhijit-Jha <abhijeetjha913@gmail.com>
2024-02-03 19:34:31 +0530

    Revert "First Commit"
    This reverts commit 9016939b6d71bfdec10da917d7d3f327276720b2.

--- Rock_paper_scissor/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-export default function Card(props){
-    
-    return<>
-        <div>
-            <img src={props.imageSrc} onClick={props.OnClick}></img>
-            <span className="font-bold">{props.title}</span>
-        </div>
-    </>
-}
